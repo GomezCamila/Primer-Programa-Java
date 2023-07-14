7e8f4d0b8e9241a530add3b194cdffedee945f6c
GomezCamila <137239132+GomezCamila@users.noreply.github.com>
2023-07-14 17:38:44 -0300

    Add files via upload

---
+++ PrimerProgramaJava/programJava.java
@@ -0,0 +1,16 @@
+public class programJava{
+public static void main(String[] args){
+
+    String nombre = "Camila Gómez";
+    int edad =  19 ;
+    String ciudad = "Tigre";
+
+    System.out.println("Me llamo "+ nombre);
+    System.out.println("Tengo "+ edad +" años de edad");
+    System.out.println("La ciudad en donde vivo es "+ ciudad);
+
+
+
+
+}
+}
